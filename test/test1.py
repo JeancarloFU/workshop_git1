0864f5391913e3806af3e427cca517d4d7908d15
JeancarloFU <jfajardou@gmail.com>
2021-05-18 11:57:38 +0200

    new dir

---
+++ test/test1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+a=np.arange(5)/10+2
+ii=np.where(a==1)
+np.min(a)
+np.max(ii)
+
+#new computations
+np.linspace(10)**2
+
+b=np.min(a)+np.mean(a)
+print("hi")
+#define new functions
+
+#end script
+
